dd3f1c7aabcecdd1e369b91eb4c3fb2098dab119
Cheastear <mr.cheesteer@gmail.com>
2024-04-26 10:50:18 +0300

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,32 @@
-function slugify(title) {
-    const arrTitle = title.toLowerCase().split(' ');
-    return arrTitle.join('-')
+function sum(first, second, ...args) {
+    let sum = first + second;
+    for (const key of args) {
+        sum += key;
+    }
+    return sum;
+}
+
+function isEnoughCapacity(products, containerSize) {
+    if (sum(...Object.values(products)) <= containerSize) {
+        return true;
+    }
+    return false;
 }
 
 console.log("task-1");
 
-console.log(slugify("Arrays for begginers"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
